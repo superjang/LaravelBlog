c72ea9145bca7eda464d242fd7d99438166f8777
장재원 <jw9651@tmon.co.kr>
2017-03-27 17:55:45 +0900

    포스트 작성 사진촬영 및 이미지 업로드 추가

--- Blog/resources/views/contents/post/add.blade.php
+++ Blog/resources/views/contents/post/add.blade.php
@@ -8,7 +8,12 @@
         <p><input type="text" name="title" placeholder="제목을 써주세용"/></p>
         <textarea name="content" id="" cols="30" rows="10">{{ old('description') }}</textarea>
         <p>
-            <input type="file" name="img" accept="image/*" capture="gallery">
+            <label for="upload">사진업로드</label>
+            <input type="file" name="img" id="upload">
+        </p>
+        <p>
+            <label for="pick">사진찍기</label>
+            <input type="file" name="img" id="pick" accept="image/*" capture="gallery">
         </p>
 
 
